a3a315de0f054f73f1bbd77327b8e2e413eec190
Brice Fotzo <bricefotzo>
2021-04-03 02:16:41 +0200

    Initial Commit

---
+++ delete.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+from utils import get_repos
+load_dotenv()
+
+#set the api link
+GITHUB_URL = os.getenv('GITHUB_URL')
+#set your github account info 
+GITHUB_USER = os.getenv('GITHUB_USER')
+GITHUB_TOKEN= os.getenv('GITHUB_TOKEN')
+
+if __name__=="__main__":
+    #get the repositories using your credentials
+    repos_dict=get_repos(GITHUB_URL,GITHUB_USER,GITHUB_TOKEN)
+    #set an input variable to choose the repositories to delete
+    to_del=input("Type the repositories id to delete (spare them with a comma): ")    
+    #split the input to create a list of repo to delete
+    repo_to_del=to_del.split(',')
+    #loop over the list of repo to delete
+    for i in repo_to_del:
+        #set the api link for deleting repo
+        url = GITHUB_URL+"/repos/"+GITHUB_USER+"/"+repos_dict[int(i)]
+        try:
+            #delete the repositories
+            response = requests.delete(url, auth=(GITHUB_USER,GITHUB_TOKEN))
+            print(response.status_code)
+        except requests.exceptions.RequestException as err:
+            raise SystemExit(err)
+
+    
