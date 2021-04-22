14eb19002b04c88abc700bcda89658490cbe22ef
Brice Fotzo <brice.fotzo@hotmail.com>
2021-04-22 18:43:20 +0200

    commit

--- add-commit-push.py
+++ add-commit-push.py
@@ -4,18 +4,19 @@ from dotenv import load_dotenv
 from utils import *
 import argparse
 
+#add argument from the command line  
 parser = argparse.ArgumentParser(description='Get the commit message.')
+#we want to add the commit message
 parser.add_argument('--commit', type=str, nargs='+',
                     help='a message for the commit')
 
+commit_message = vars(parser.parse_args())['commit'][0]
 
-args = parser.parse_args()
-# print(vars(args)['commit'][0])
 if __name__=="__main__":
     print("-------------------- ADD --------------------")
     os.system("git add .")
     print("-------------------- COMMIT --------------------")
-    os.system('git commit -m "{}"'.format(vars(args)['commit'][0]))
+    os.system('git commit -m "{}"'.format(commit_message))
     print("-------------------- PUSH --------------------")
     os.system("git push")
     
